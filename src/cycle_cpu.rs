2f3757699a02429f1c35b12e2def452e3da34050
Nicholas Flynt <thegamefreak0134@gmail.com>
2017-10-03 19:06:14 -0500

    Implement absolute read / write addressing modes in new cycle CPU

--- src/cycle_cpu.rs
+++ src/cycle_cpu.rs
@@ -1,6 +1,7 @@
 use cpu;
 use cpu::Registers;
 use memory::read_byte;
+use memory::write_byte;
 use nes::NesState;
 use opcodes;
 
@@ -8,6 +9,7 @@ pub struct CpuState {
   pub tick: u8,
   pub opcode: u8,
   pub data1: u8,
+  pub data2: u8,
 }
 
 impl CpuState {
@@ -16,6 +18,7 @@ impl CpuState {
       tick: 1,
       opcode: 0,
       data1: 0,
+      data2: 0,
     }
   }
 }
@@ -53,12 +56,53 @@ pub fn unimplemented_rmw(nes: &mut NesState, _: RmwOpcode) {
   nes.cpu.tick = 1;
 }
 
+// Immediate mode only supports reading data
 pub fn immediate_read(nes: &mut NesState, opcode_func: ReadOpcode) {
   let data = nes.cpu.data1;
   opcode_func(&mut nes.registers, data);
   nes.registers.pc = nes.registers.pc.wrapping_add(1);
   nes.cpu.tick = 1;
 }
+
+// Absolute mode
+pub fn absolute_read(nes: &mut NesState, opcode_func: ReadOpcode) {
+  match nes.cpu.tick {
+    2 => {
+      // data1 is already filled
+      nes.registers.pc = nes.registers.pc.wrapping_add(1);}
+    3 => {
+      let pc = nes.registers.pc;
+      nes.cpu.data2 = read_byte(nes, pc);
+      nes.registers.pc = nes.registers.pc.wrapping_add(1);}
+    4 => {
+      let address = ((nes.cpu.data2 as u16) << 8) | (nes.cpu.data1 as u16);
+      let data = read_byte(nes, address);
+      opcode_func(&mut nes.registers, data);
+      nes.cpu.tick = 1;
+    },
+    _ => ()
+  }
+}
+
+pub fn absolute_write(nes: &mut NesState, opcode_func: WriteOpcode) {
+  match nes.cpu.tick {
+    2 => {
+      // data1 is already filled
+      nes.registers.pc = nes.registers.pc.wrapping_add(1);}
+    3 => {
+      let pc = nes.registers.pc;
+      nes.cpu.data2 = read_byte(nes, pc);
+      nes.registers.pc = nes.registers.pc.wrapping_add(1);}
+    4 => {
+      let address = ((nes.cpu.data2 as u16) << 8) | (nes.cpu.data1 as u16);
+      let data = opcode_func(&mut nes.registers);
+      write_byte(nes, address, data);
+      nes.cpu.tick = 1;
+    },
+    _ => ()
+  }
+}
+
 
 // Called by STA in #imm mode, this has the effect of a two-byte NOP
 // which skips the data byte. but still takes just 2 cycles.
@@ -102,6 +146,11 @@ pub fn run_one_clock(nes: &mut NesState) {
         0b010 => AddressingMode{
           read: immediate_read, 
           write: nop_write, 
+          rmw: unimplemented_rmw},
+        // Absolute Mode
+        0x011 => AddressingMode{
+          read: absolute_read,
+          write: absolute_write,
           rmw: unimplemented_rmw},
         // Not implemented yet
         _ => AddressingMode{
