d5c64011bd030986613a363784679d5c190ec365
Nicholas Flynt <thegamefreak0134@gmail.com>
2017-10-14 13:18:46 -0500

    Rewrite background drawing using more correct timing
    This is the start of a rather major PPU rewrite. Sprites are broken,
    known regressions include every game ever released and probably some
    that weren't.

--- src/ppu.rs
+++ src/ppu.rs
@@ -33,9 +33,7 @@ pub struct PpuState {
     // Internal
     pub current_frame: u32,
     pub current_scanline: u16,
-    pub scanline_cycles: u64,
-    pub last_cycle: u64,
-    // 
+    pub current_scanline_cycle: u16,    
 
     // Framebuffer
     pub screen: Vec<u8>,
@@ -51,42 +49,59 @@ pub struct PpuState {
     pub temporary_vram_address: u16,
     pub fine_x: u8,
     pub fine_y: u8,
+    pub tile_shift_low: u16,
+    pub tile_shift_high: u16,
+    pub tile_low: u8,
+    pub tile_high: u8,
+    pub tile_index: u8,
+    pub palette_shift_low: u8,
+    pub palette_shift_high: u8,
+    pub palette_latch: u8,
+    pub attribute_byte: u8,
 }
 
 impl PpuState {
     pub fn new() -> PpuState {
         return PpuState {
-           internal_vram: vec!(0u8; 0x1000),  // 4k for four-screen mirroring, most games only use upper 2k
-           oam: vec!(0u8; 0x100),
-           palette: vec!(0u8; 0x20),
-           current_frame: 0,
-           current_scanline: 0,
-           scanline_cycles: 0,
-           last_cycle: 0,
-           screen: vec!(0u8; 256 * 240),
-           sprite_color: vec!(0u8; 256),
-           sprite_index: vec!(0u8; 256),
-           sprite_bg_priority: vec!(false; 256),
-           sprite_zero: vec!(false; 256),
-
-           control: 0,
-           mask: 0,
-           status: 0,
-           oam_addr: 0,
-           scroll_x: 0,
-           scroll_y: 0,
-           current_addr: 0,
-           oam_dma_high: 0,
-           latch: 0,
-           read_buffer: 0,
-
-           write_toggle: false,
-
-           // Internal State
-           current_vram_address: 0,
-           temporary_vram_address: 0,
-           fine_x: 0,
-           fine_y: 0,
+            internal_vram: vec!(0u8; 0x1000),  // 4k for four-screen mirroring, most games only use upper 2k
+            oam: vec!(0u8; 0x100),
+            palette: vec!(0u8; 0x20),
+            current_frame: 0,
+            current_scanline: 0,
+            current_scanline_cycle: 0,
+            screen: vec!(0u8; 256 * 240),
+            sprite_color: vec!(0u8; 256),
+            sprite_index: vec!(0u8; 256),
+            sprite_bg_priority: vec!(false; 256),
+            sprite_zero: vec!(false; 256),
+    
+            control: 0,
+            mask: 0,
+            status: 0,
+            oam_addr: 0,
+            scroll_x: 0,
+            scroll_y: 0,
+            current_addr: 0,
+            oam_dma_high: 0,
+            latch: 0,
+            read_buffer: 0,
+    
+            write_toggle: false,
+    
+            // Internal State
+            current_vram_address: 0,
+            temporary_vram_address: 0,
+            fine_x: 0,
+            fine_y: 0,
+            tile_shift_low: 0,
+            tile_shift_high: 0,
+            tile_low: 0,
+            tile_high: 0,
+            tile_index: 0,
+            palette_shift_low: 0,
+            palette_shift_high: 0,
+            palette_latch: 0,
+            attribute_byte: 0,
        };
     }
 
@@ -289,7 +304,7 @@ impl PpuState {
     }
 
     // Return value: NMI interrupt happened this cycle
-    pub fn process_scanline(&mut self, mapper: &mut Mapper) {
+    /*pub fn process_scanline(&mut self, mapper: &mut Mapper) {
         let scanline = self.current_scanline;
         match scanline {
             0 ... 239 => {
@@ -311,19 +326,242 @@ impl PpuState {
             },
             _ => ()
         }
+    }*/
+
+    pub fn rendering_enabled(&self) -> bool {
+        return (self.mask & 0b0001_1000) != 0;
+    }
+
+    fn shift_bg_registers(&mut self) {
+        self.tile_shift_high = self.tile_shift_high << 1;
+        self.tile_shift_low = self.tile_shift_low << 1;
+        self.palette_shift_high = self.palette_shift_high << 1;
+        self.palette_shift_low = self.palette_shift_low << 1;
+        // Palette data needs to be reloaded from the current latch
+        self.palette_shift_high |= (self.palette_latch & 0b10) >> 1;
+        self.palette_shift_low  |=  self.palette_latch & 0b01;
+    }
+
+    fn reload_shift_registers(&mut self) {
+        self.tile_shift_high &= 0xFF00;
+        self.tile_shift_high |= self.tile_high as u16;
+        self.tile_shift_low &= 0xFF00;
+        self.tile_shift_low |= self.tile_low as u16;
+        // Use coarse X and coarse Y (tile indices) to determine which palette to use from
+        // the attribute byte, and apply that to the palette latch
+        //                                          nn yyyyy xxxxx
+        let attr_x =  self.current_vram_address & 0b00_00000_00001;
+        let attr_y = (self.current_vram_address & 0b00_00001_00000) >> 5;
+        let palette_shift = ((attr_y << 1) | attr_x) * 2;
+        self.palette_latch = (self.attribute_byte >> palette_shift) & 0b11;
+    }
+
+    fn draw_pixel(&mut self, mapper: &mut Mapper) {
+        // Output a pixel based on the current background shifters
+        let bg_x_bit = 0b1000_0000_0000_0000 >> self.fine_x;
+        let bg_x_shift = 15 - self.fine_x;
+        let palette_index = 
+            (self.tile_shift_high & bg_x_bit) >> (bg_x_shift - 1) | 
+            (self.tile_shift_low & bg_x_bit) >> bg_x_shift;
+
+        let attr_x_bit = 0b1000_0000 >> self.fine_x;
+        let attr_x_shift = 15 - self.fine_x;
+        let palette_number =
+            (self.palette_shift_high & attr_x_bit) >> (attr_x_shift - 1) | 
+            (self.palette_shift_low & attr_x_bit) >> attr_x_shift;
+
+        let bg_color = self._read_byte(mapper, (((palette_number as u16) << 2) + palette_index) as u16 + 0x3F00);
+
+        // TODO: Include sprites here
+        self.screen[((self.current_scanline * 256) + (self.current_scanline_cycle - 1)) as usize] = bg_color;
+    }
+
+    fn increment_coarse_x(&mut self) {
+        let mut coarse_x = self.current_vram_address & 0b00_00000_11111;
+        coarse_x += 1;
+        // If we overflowed Coarse X then
+        if coarse_x > 0b11111 {
+            // Switch to the adjacent horizontal nametable
+            self.current_vram_address ^= 0b01_00000_00000;
+        }
+        self.current_vram_address = self.current_vram_address & 0b11_11111_00000 | (coarse_x & 0b11111);
+    }
+
+    fn increment_coarse_y(&mut self) {
+        let mut coarse_y = (self.current_vram_address & 0b00_11111_00000) >> 5;
+        coarse_y += 1;
+        if coarse_y == 29 {
+            coarse_y = 0;
+            self.current_vram_address ^= 0b10_00000_00000;
+        }
+        self.current_vram_address = self.current_vram_address & 0b11_00000_11111 | ((coarse_y & 0b11111) << 5);
+    }
+
+    fn increment_fine_y(&mut self) {
+        self.fine_y += 1;
+        if self.fine_y > 7 {
+            self.fine_y = 0;
+            self.increment_coarse_y();
+        }
+    }
+
+    fn prerender_scanline(&mut self, mapper: &mut Mapper,) {
+        // Setup for next full frame
+        match self.current_scanline_cycle {
+            1 => {
+                // Clear vblank, sprite overflow and sprite zero hit
+                self.status = self.status & 0x1F;
+            },
+            257 => {
+                if self.rendering_enabled() {
+                    // Reload the X scroll components
+                    self.current_vram_address &= 0b10_11111_00000;
+                    self.current_vram_address |= self.temporary_vram_address & 0b01_00000_11111;
+                }
+            }
+            280 ... 304 => {
+                if self.rendering_enabled() {
+                    // Reload the Y scroll components
+                    self.current_vram_address &= 0b01_00000_11111;
+                    self.current_vram_address |= self.temporary_vram_address & 0b10_11111_00000;
+                    self.fine_y = ((self.temporary_vram_address & 0b111_00_00000_00000) >> 12) as u8;
+                }
+            }
+            321 ... 336 => {
+                if self.rendering_enabled() {
+                    self.shift_bg_registers();
+                    // Fetch nametable tiles for the first two tiles on the next scanline
+                    let sub_cycle = (self.current_scanline_cycle - 321) % 8;
+                    self.fetch_bg_tile(mapper, sub_cycle);
+                }
+            },
+            // Dummy fetch nametable address for next scanline x2
+            // (Required for MMU5 to detect scanlines for IRQs, among other things.)
+            337 => {
+                if self.rendering_enabled() {
+                    let tile_address = 0x2000 | (self.current_vram_address & 0x0FFF);
+                    self.tile_index = self._read_byte(mapper, tile_address);
+                }
+            },
+            339 => {
+                if self.rendering_enabled() {
+                    let tile_address = 0x2000 | (self.current_vram_address & 0x0FFF);
+                    self.tile_index = self._read_byte(mapper, tile_address);
+
+                    if self.current_frame & 0x1 != 0 {
+                        // Skip ahead one cycle on odd frames. This jitter produces a cleaner image
+                        // for NTSC signal generation.
+                        self.current_scanline_cycle = 340;
+                    }
+                }
+            },
+            _ => ()
+        }
+    }
+
+    fn fetch_bg_tile(&mut self, mapper: &mut Mapper, sub_cycle: u16) {
+        let mut pattern_address: u16 = 0x0000;
+        if (self.control & 0x10) != 0 {
+            pattern_address = 0x1000;
+        }
+
+        match sub_cycle {
+        // Documentation for these addresses: https://wiki.nesdev.com/w/index.php/PPU_scrolling#Tile_and_attribute_fetching
+        // Note that mirroring is applied in _read_byte, not here.
+        0 => {
+            let tile_address = 0x2000 | (self.current_vram_address & 0x0FFF);
+            self.tile_index = self._read_byte(mapper, tile_address);
+        },
+        2 => {
+            let attribute_address = 
+                0x23C0 | 
+                 (self.current_vram_address & 0x0C00) | 
+                ((self.current_vram_address >> 4) & 0x38) | 
+                ((self.current_vram_address >> 2) & 0x07);
+            self.attribute_byte = self._read_byte(mapper, attribute_address);
+        },
+        4 => {
+            let tile_low_address = pattern_address + 
+                (self.tile_index as u16 * 16) + 
+                 self.fine_y as u16;
+            self.tile_low = self._read_byte(mapper, tile_low_address);
+        },
+        6 => {
+            let tile_high_address = pattern_address + 
+                (self.tile_index as u16 * 16) + 8 +
+                 self.fine_y as u16;
+            self.tile_high = self._read_byte(mapper, tile_high_address);
+        },
+        7 => {
+            self.reload_shift_registers();
+            self.increment_coarse_x();
+        },
+        _ => ()
+    }
+    }
+
+    fn render_scanline(&mut self, mapper: &mut Mapper) {
+        if self.rendering_enabled() {
+            match self.current_scanline_cycle {
+                // cycle 0 is a dummy cycle, nothing happens
+                1 ... 256 => {
+                    self.draw_pixel(mapper);
+                    self.shift_bg_registers();
+                    let sub_cycle = (self.current_scanline_cycle - 1) % 8;
+                    self.fetch_bg_tile(mapper, sub_cycle);
+                    
+                    if self.current_scanline_cycle == 256 {
+                        self.increment_fine_y();
+                    }
+                },
+                257 ... 320 => {
+                    if self.current_scanline_cycle == 257 {
+                        // Reload the X scroll components
+                        self.current_vram_address &= 0b10_11111_00000;
+                        self.current_vram_address |= self.temporary_vram_address & 0b01_00000_11111;
+                    }
+                    // Sprite tile fetches
+                },
+                321 ... 336 => {
+                    self.shift_bg_registers();
+                    // Fetch nametable tiles for the first two tiles on the next scanline
+                    let sub_cycle = (self.current_scanline_cycle - 321) % 8;
+                    self.fetch_bg_tile(mapper, sub_cycle);
+                },
+                // Dummy fetch nametable address for next scanline x2
+                // (Required for MMU5 to detect scanlines for IRQs, among other things.)
+                337 | 339 => {
+                    let tile_address = 0x2000 | (self.current_vram_address & 0x0FFF);
+                    self.tile_index = self._read_byte(mapper, tile_address);
+                },
+                _ => ()
+            }
+        }
+    }
+
+    fn vblank_scanline(&mut self) {
+        if self.current_scanline_cycle == 1 {
+            // VBlank! Set NMI flag here
+            self.status = (self.status & 0x7F) + 0x80;
+        }
     }
 
     pub fn clock(&mut self, mapper: &mut Mapper) {
-        let cycles_per_scanline = 341;
-        self.scanline_cycles = self.scanline_cycles + 1;
-        while self.scanline_cycles > cycles_per_scanline {
-            self.process_scanline(mapper);
-            self.current_scanline = self.current_scanline.wrapping_add(1);
+        match self.current_scanline {
+            0 ... 239 => self.render_scanline(mapper),
+            241 => self.vblank_scanline(),
+            261 => self.prerender_scanline(mapper),
+            _ => ()
+        }
+
+        self.current_scanline_cycle += 1;
+        if self.current_scanline_cycle > 340 {
+            self.current_scanline_cycle = 0;
+            self.current_scanline += 1;
             if self.current_scanline > 261 {
                 self.current_scanline = 0;
-                self.current_frame = self.current_frame + 1;
+                self.current_frame += 1;
             }
-            self.scanline_cycles = self.scanline_cycles  - cycles_per_scanline;
         }
     }
 
