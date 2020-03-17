97926fbbaee59ba0223db867d79e3ac287220512
Nicholas Flynt <nicholas.flynt@rackspace.com>
2020-03-17 18:26:45 -0500

    Add in-mapper mirroring to NROM

--- src/mmc/nrom.rs
+++ src/mmc/nrom.rs
@@ -3,6 +3,7 @@
 
 use cartridge::NesHeader;
 use mmc::mapper::*;
+use mmc::mirroring;
 
 pub struct Nrom {
     pub prg_rom: Vec<u8>,
@@ -10,6 +11,7 @@ pub struct Nrom {
     pub chr_rom: Vec<u8>,
     pub mirroring: Mirroring,
     pub has_chr_ram: bool,
+    pub vram: Vec<u8>,
 }
 
 impl Nrom {
@@ -25,11 +27,18 @@ impl Nrom {
             chr_rom: chr_rom,
             mirroring: header.mirroring,
             has_chr_ram: header.has_chr_ram,
+            vram: vec![0u8; 0x1000],
         }
     }
 }
 
 impl Mapper for Nrom {
+        fn print_debug_status(&self) {
+        println!("======= NROM =======");
+        println!("Mirroring Mode: {}", mirroring_mode_name(self.mirroring));
+        println!("====================");
+    }
+
     fn mirroring(&self) -> Mirroring {
         return self.mirroring;
     }
@@ -67,6 +76,11 @@ impl Mapper for Nrom {
     fn read_ppu(&mut self, address: u16) -> Option<u8> {
         match address {
             0x0000 ... 0x1FFF => return Some(self.chr_rom[address as usize]),
+            0x2000 ... 0x3FFF => return match self.mirroring {
+                Mirroring::Horizontal => Some(self.vram[mirroring::horizontal_mirroring(address) as usize]),
+                Mirroring::Vertical   => Some(self.vram[mirroring::vertical_mirroring(address) as usize]),
+                _ => None
+            },
             _ => return None
         }
     }
@@ -77,6 +91,11 @@ impl Mapper for Nrom {
                 if self.has_chr_ram {
                     self.chr_rom[address as usize] = data;
                 }
+            },
+            0x2000 ... 0x3FFF => match self.mirroring {
+                Mirroring::Horizontal => self.vram[mirroring::horizontal_mirroring(address) as usize] = data,
+                Mirroring::Vertical   => self.vram[mirroring::vertical_mirroring(address) as usize] = data,
+                _ => {}
             },
             _ => {}
         }
