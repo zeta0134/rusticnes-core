0df550d50f9d72515b544737ffd44a79cd0e7e9c
Nicholas Flynt <nicholas.flynt@rackspace.com>
2020-03-17 19:53:40 -0500

    Give UxROM the in-mapper nametable treatment
    ...whew! That's all of them.

--- src/mmc/uxrom.rs
+++ src/mmc/uxrom.rs
@@ -3,12 +3,14 @@
 
 use cartridge::NesHeader;
 use mmc::mapper::*;
+use mmc::mirroring;
 
 pub struct UxRom {
     pub prg_rom: Vec<u8>,
     pub chr_ram: Vec<u8>,
     pub mirroring: Mirroring,
     pub prg_bank: usize,
+    pub vram: Vec<u8>,
 }
 
 impl UxRom {
@@ -18,11 +20,19 @@ impl UxRom {
             chr_ram: vec![0u8; 0x2000],
             mirroring: header.mirroring,
             prg_bank: 0x00,
+            vram: vec![0u8; 0x1000],
         }
     }
 }
 
 impl Mapper for UxRom {
+    fn print_debug_status(&self) {
+        println!("======= UxROM =======");
+        println!("PRG Bank: {}, ", self.prg_bank);
+        println!("Mirroring Mode: {}", mirroring_mode_name(self.mirroring));
+        println!("====================");
+    }
+
     fn mirroring(&self) -> Mirroring {
         return self.mirroring;
     }
@@ -52,6 +62,11 @@ impl Mapper for UxRom {
     fn read_ppu(&mut self, address: u16) -> Option<u8> {
         match address {
             0x0000 ... 0x1FFF => return Some(self.chr_ram[address as usize]),
+            0x2000 ... 0x3FFF => return match self.mirroring {
+                Mirroring::Horizontal => Some(self.vram[mirroring::horizontal_mirroring(address) as usize]),
+                Mirroring::Vertical   => Some(self.vram[mirroring::vertical_mirroring(address) as usize]),
+                _ => None
+            },
             _ => return None
         }
     }
@@ -59,6 +74,11 @@ impl Mapper for UxRom {
     fn write_ppu(&mut self, address: u16, data: u8) {
         match address {
             0x0000 ... 0x1FFF => self.chr_ram[address as usize] = data,
+            0x2000 ... 0x3FFF => match self.mirroring {
+                Mirroring::Horizontal => self.vram[mirroring::horizontal_mirroring(address) as usize] = data,
+                Mirroring::Vertical   => self.vram[mirroring::vertical_mirroring(address) as usize] = data,
+                _ => {}
+            },
             _ => {}
         }
     }
