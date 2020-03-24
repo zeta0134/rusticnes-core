ef87216dfc8bd12c5ee57ee11749a4945c29a78e
Nicholas Flynt <nicholas.flynt@rackspace.com>
2020-03-24 11:04:29 -0500

    nt_offset -> NT_OFFSET

--- src/mmc/mirroring.rs
+++ src/mmc/mirroring.rs
@@ -1,14 +1,17 @@
-const nt_offset: (u16, u16, u16, u16) = (0x000, 0x400, 0x800, 0xC00);
+// Set of helper functions to assist mappers with a few of the most
+// common mirroring modes. Less common mirroring modes and more complex
+// logic may still be implemented within individual mappers as needed.
+const NT_OFFSET: (u16, u16, u16, u16) = (0x000, 0x400, 0x800, 0xC00);
 
 pub fn horizontal_mirroring(read_address: u16) -> u16 {
     let nt_base = read_address & 0xFFF;
     let nt_address = read_address & 0x3FF;
     match nt_base {
         // Nametable 0 (top-left)
-        0x000 ... 0x3FF => nt_address + nt_offset.0,
-        0x400 ... 0x7FF => nt_address + nt_offset.0,
-        0x800 ... 0xBFF => nt_address + nt_offset.1,
-        0xC00 ... 0xFFF => nt_address + nt_offset.1,
+        0x000 ... 0x3FF => nt_address + NT_OFFSET.0,
+        0x400 ... 0x7FF => nt_address + NT_OFFSET.0,
+        0x800 ... 0xBFF => nt_address + NT_OFFSET.1,
+        0xC00 ... 0xFFF => nt_address + NT_OFFSET.1,
         _ => return 0, // wat
     }
 }
@@ -18,10 +21,10 @@ pub fn vertical_mirroring(read_address: u16) -> u16 {
     let nt_address = read_address & 0x3FF;
     match nt_base {
         // Nametable 0 (top-left)
-        0x000 ... 0x3FF => nt_address + nt_offset.0,
-        0x400 ... 0x7FF => nt_address + nt_offset.1,
-        0x800 ... 0xBFF => nt_address + nt_offset.0,
-        0xC00 ... 0xFFF => nt_address + nt_offset.1,
+        0x000 ... 0x3FF => nt_address + NT_OFFSET.0,
+        0x400 ... 0x7FF => nt_address + NT_OFFSET.1,
+        0x800 ... 0xBFF => nt_address + NT_OFFSET.0,
+        0xC00 ... 0xFFF => nt_address + NT_OFFSET.1,
         _ => return 0, // wat
     }
 }
@@ -31,10 +34,10 @@ pub fn one_screen_lower(read_address: u16) -> u16 {
     let nt_address = read_address & 0x3FF;
     match nt_base {
         // Nametable 0 (top-left)
-        0x000 ... 0x3FF => nt_address + nt_offset.0,
-        0x400 ... 0x7FF => nt_address + nt_offset.0,
-        0x800 ... 0xBFF => nt_address + nt_offset.0,
-        0xC00 ... 0xFFF => nt_address + nt_offset.0,
+        0x000 ... 0x3FF => nt_address + NT_OFFSET.0,
+        0x400 ... 0x7FF => nt_address + NT_OFFSET.0,
+        0x800 ... 0xBFF => nt_address + NT_OFFSET.0,
+        0xC00 ... 0xFFF => nt_address + NT_OFFSET.0,
         _ => return 0, // wat
     }
 }
@@ -44,10 +47,10 @@ pub fn one_screen_upper(read_address: u16) -> u16 {
     let nt_address = read_address & 0x3FF;
     match nt_base {
         // Nametable 0 (top-left)
-        0x000 ... 0x3FF => nt_address + nt_offset.1,
-        0x400 ... 0x7FF => nt_address + nt_offset.1,
-        0x800 ... 0xBFF => nt_address + nt_offset.1,
-        0xC00 ... 0xFFF => nt_address + nt_offset.1,
+        0x000 ... 0x3FF => nt_address + NT_OFFSET.1,
+        0x400 ... 0x7FF => nt_address + NT_OFFSET.1,
+        0x800 ... 0xBFF => nt_address + NT_OFFSET.1,
+        0xC00 ... 0xFFF => nt_address + NT_OFFSET.1,
         _ => return 0, // wat
     }
 }
@@ -57,10 +60,10 @@ pub fn four_banks(read_address: u16) -> u16 {
     let nt_address = read_address & 0x3FF;
     match nt_base {
         // Nametable 0 (top-left)
-        0x000 ... 0x3FF => nt_address + nt_offset.0,
-        0x400 ... 0x7FF => nt_address + nt_offset.1,
-        0x800 ... 0xBFF => nt_address + nt_offset.2,
-        0xC00 ... 0xFFF => nt_address + nt_offset.3,
+        0x000 ... 0x3FF => nt_address + NT_OFFSET.0,
+        0x400 ... 0x7FF => nt_address + NT_OFFSET.1,
+        0x800 ... 0xBFF => nt_address + NT_OFFSET.2,
+        0xC00 ... 0xFFF => nt_address + NT_OFFSET.3,
         _ => return 0, // wat
     }
 }
