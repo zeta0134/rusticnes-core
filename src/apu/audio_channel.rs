4ba3f389cd5e56b6f9ccc28f296680a197e41174
Nicholas Flynt <thegamefreak0134@gmail.com>
2021-02-07 16:10:02 -0500

    Introduce abstractions for audio channel semantics
    Basically, for any given channel, I want to be able to report simple
    frequency, volume, and patch data, for use with visualizers. These
    are not meant to be fully accurate with respect to all of the
    raw channel data (for that, specilize on the Mapper and read the
    registers directly) but more useful for high level renderings.

--- src/apu/audio_channel.rs
+++ src/apu/audio_channel.rs
@@ -4,6 +4,25 @@
 
 use super::RingBuffer;
 
+#[derive(Clone)]
+pub enum PlaybackRate {
+    FundamentalFrequency { frequency: f64 },
+    LfsrRate { index: usize, max: usize },
+    SampleRate { frequency: f64 },
+}
+
+#[derive(Clone)]
+pub enum Volume {
+    VolumeIndex { index: usize, max: usize },
+}
+
+#[derive(Clone)]
+pub enum Timbre {
+    DutyIndex { index: usize, max: usize },
+    LsfrMode { index: usize, max: usize },
+    PatchIndex { index: usize, max: usize },
+}
+
 pub trait AudioChannelState {
     fn name(&self) -> String;
     fn sample_buffer(&self) -> &RingBuffer;
@@ -13,4 +32,9 @@ pub trait AudioChannelState {
     fn muted(&self) -> bool;
     fn mute(&mut self);
     fn unmute(&mut self);
+
+    fn playing(&self) -> bool { return false; }
+    fn rate(&self) -> PlaybackRate { return PlaybackRate::SampleRate{frequency: 0.0}; }
+    fn volume(&self) -> Option<Volume> {return None}
+    fn timbre(&self) -> Option<Timbre> {return None}
 }
