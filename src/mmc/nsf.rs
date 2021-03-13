817345edc44fc31c9ad6f3965fa430109caf1c67
Nicholas Flynt <thegamefreak0134@gmail.com>
2021-03-13 18:22:02 -0500

    Implement bank switching, and also shim non-bankswitched files
    For now we are entirely ignoring the FDS system, which means we
    are also ignoring the banks in 0x6000; this is why the load
    address will be no lower than 0x8000 for testing. Later we can
    extend that to 0x6000 once the relevant banks are implemented.

--- src/mmc/nsf.rs
+++ src/mmc/nsf.rs
@@ -16,6 +16,12 @@ pub struct NsfMapper {
     chr: Vec<u8>,
     nsf_player: Vec<u8>,
 
+    prg_rom_banks: Vec<usize>,
+
+    // YOU WERE HERE. Next steps: implement banking, so the call to init_address reaches real code. Then,
+    // see if that code executes and returns correctly; you should see a spinwait on page 0x5000, and might
+    // also see some RAM values set.
+
     mirroring: Mirroring,
     vram: Vec<u8>,
 }
@@ -26,6 +32,9 @@ const PPUSTATUS: u16 = 0x2002;
 const PPUSCROLL: u16 = 0x2005;
 const PPUADDR: u16 = 0x2006;
 const PPUDATA: u16 = 0x2007;
+
+const APUSTATUS: u16 = 0x4015;
+const APUFRAMECTRL: u16 = 0x4017;
 
 const COLOR_BLACK: u8 = 0x0F;
 const COLOR_WHITE: u8 = 0x30;
@@ -67,10 +76,10 @@ fn nsf_player(init_address: u16) -> Vec<Opcode> {
 
         // Enable all channels)
         Lda(Immediate(0x0F)),
-        Sta(Absolute(0x4015)),
+        Sta(Absolute(APUSTATUS)),
         // Set the frame counter to 4-step mode
         Lda(Immediate(0x40)),
-        Sta(Absolute(0x4017)),
+        Sta(Absolute(APUFRAMECTRL)),
         // (bank initialization is handled by the mapper)
         // Load the first song index to A
         Lda(Immediate(0x00)),
@@ -91,10 +100,28 @@ impl NsfMapper {
         let mut nsf_player = assemble(Vec::from(nsf_player(nsf.header.init_address())))?;
         nsf_player.resize(0x1000, 0);
 
+        let mut prg_rom = nsf.prg.clone();
+        let mut prg_rom_banks = nsf.header.initial_banks();
+        if !nsf.header.is_bank_switched() {
+            if nsf.header.load_address() <= 0x8000 {
+                return Err(format!("Load address {} is below 0x8000, this conflicts with player implementation. Refusing to load.", nsf.header.load_address()));
+            }
+
+            // Coerce this ROM into a bank switched format anyway, so the mapper logic becomes simplified
+            let mut padded_rom: Vec<u8> = Vec::new();
+            padded_rom.resize((nsf.header.load_address() as usize) - 0x8000, 0);
+            padded_rom.extend(prg_rom);
+            padded_rom.resize(0x8000, 0);
+            prg_rom = padded_rom;
+            prg_rom_banks = vec![0, 1, 2, 3, 4, 5, 6, 7];
+        }
+
         return Ok(NsfMapper {
-            prg: MemoryBlock::new(&nsf.prg, MemoryType::Ram),
+            prg: MemoryBlock::new(&prg_rom, MemoryType::Ram),
             chr: vec![0u8; 0x2000],
             nsf_player: nsf_player,
+
+            prg_rom_banks: prg_rom_banks,
 
             mirroring: Mirroring::FourScreen,
             vram: vec![0u8; 0x1000],
@@ -110,6 +137,14 @@ impl Mapper for NsfMapper {
     fn debug_read_cpu(&self, address: u16) -> Option<u8> {
         match address {
             0x5000 ..= 0x5FFF => Some(self.nsf_player[(address - 0x5000) as usize]),
+            0x8000 ..= 0x8FFF => self.prg.banked_read(0x1000, self.prg_rom_banks[0], (address - 0x8000) as usize),
+            0x9000 ..= 0x9FFF => self.prg.banked_read(0x1000, self.prg_rom_banks[1], (address - 0x9000) as usize),
+            0xA000 ..= 0xAFFF => self.prg.banked_read(0x1000, self.prg_rom_banks[2], (address - 0xA000) as usize),
+            0xB000 ..= 0xBFFF => self.prg.banked_read(0x1000, self.prg_rom_banks[3], (address - 0xB000) as usize),
+            0xC000 ..= 0xCFFF => self.prg.banked_read(0x1000, self.prg_rom_banks[4], (address - 0xC000) as usize),
+            0xD000 ..= 0xDFFF => self.prg.banked_read(0x1000, self.prg_rom_banks[5], (address - 0xD000) as usize),
+            0xE000 ..= 0xEFFF => self.prg.banked_read(0x1000, self.prg_rom_banks[6], (address - 0xE000) as usize),
+            0xF000 ..= 0xFFF9 => self.prg.banked_read(0x1000, self.prg_rom_banks[7], (address - 0xF000) as usize),
             0xFFFC => {Some(0x00)}, // reset vector
             0xFFFD => {Some(0x50)},
             _ => None
@@ -118,7 +153,14 @@ impl Mapper for NsfMapper {
 
     fn write_cpu(&mut self, address: u16, data: u8) {
         match address {
-            /* complicated! */
+            0x5FF8 => {self.prg_rom_banks[0] = data as usize},
+            0x5FF9 => {self.prg_rom_banks[1] = data as usize},
+            0x5FFA => {self.prg_rom_banks[2] = data as usize},
+            0x5FFB => {self.prg_rom_banks[3] = data as usize},
+            0x5FFC => {self.prg_rom_banks[4] = data as usize},
+            0x5FFD => {self.prg_rom_banks[5] = data as usize},
+            0x5FFE => {self.prg_rom_banks[6] = data as usize},
+            0x5FFF => {self.prg_rom_banks[7] = data as usize},
             _ => {}
         }
     }
