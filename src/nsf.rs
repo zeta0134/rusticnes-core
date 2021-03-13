3252a9026c2721be82ab87168118cfa82819bfee
Nicholas Flynt <thegamefreak0134@gmail.com>
2021-03-12 23:23:52 -0500

    Implement prg length and bank initialization

--- src/nsf.rs
+++ src/nsf.rs
@@ -20,8 +20,12 @@ const NSF_PLAY_ADDR: usize = 0x00C;
 //const NSF_ARTIST_NAME: usize = 0x02E;
 //const NSF_COPYRIGHT_HOLDER: usize = 0x04E;
 const NSF_NTSC_PLAY_SPEED: usize = 0x06E;
-//const NSF_BANK_INIT: usize = 0x070;
+const NSF_BANK_INIT: usize = 0x070;
 const NSF_PAL_PLAY_SPEED: usize = 0x078;
+//const NSF_NTSC_PAL_SELECTION: usize = 0x07A;
+//const NSF_EXPANSION_CHIPS: usize = 0x07B;
+//const NSF2_FLAGS: usize = 0x07C;
+const NSF_PRG_LENGTH: usize = 0x07D;
 
 impl NsfHeader {
     pub fn from(raw_bytes: &[u8]) -> NsfHeader {
@@ -81,7 +85,23 @@ impl NsfHeader {
         return self._word(NSF_PAL_PLAY_SPEED);
     }
 
+    pub fn initial_banks(&self) -> Vec<usize> {
+        return vec![
+            self.raw_bytes[NSF_BANK_INIT + 0] as usize,
+            self.raw_bytes[NSF_BANK_INIT + 1] as usize,
+            self.raw_bytes[NSF_BANK_INIT + 2] as usize,
+            self.raw_bytes[NSF_BANK_INIT + 3] as usize,
+            self.raw_bytes[NSF_BANK_INIT + 4] as usize,
+            self.raw_bytes[NSF_BANK_INIT + 5] as usize,
+            self.raw_bytes[NSF_BANK_INIT + 6] as usize,
+            self.raw_bytes[NSF_BANK_INIT + 7] as usize,
+        ];
+    }
 
-
-
+    pub fn program_length(&self) -> usize {
+        let addr_low =   self.raw_bytes[NSF_PRG_LENGTH + 0] as usize;
+        let addr_mid =  (self.raw_bytes[NSF_PRG_LENGTH + 1] as usize) << 8;
+        let addr_high = (self.raw_bytes[NSF_PRG_LENGTH + 2] as usize) << 16;
+        return addr_low + addr_mid + addr_high;
+    }
 }
