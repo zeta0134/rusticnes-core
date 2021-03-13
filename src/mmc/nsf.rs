89ad23b15eace0526d9535d0fc9a7725e4d0b729
Nicholas Flynt <thegamefreak0134@gmail.com>
2021-03-13 10:27:18 -0500

    Assemble a *very* basic test program, and hook it up at 0x5000
    This works! Now we need to expand on the assembler a bit, and then
    flesh out the program to do some useful initialization work for
    the player.

--- src/mmc/nsf.rs
+++ src/mmc/nsf.rs
@@ -2,26 +2,37 @@
 // player, so it will have some inherent limitations similar to most flashcarts.
 // Reference capabilities: https://wiki.nesdev.com/w/index.php/NSF
 
-use nsf::NsfFile;
+use asm::*;
+use asm::Opcode::*;
+use asm::AddressingMode::*;
 use memoryblock::MemoryBlock;
 use memoryblock::MemoryType;
-
 use mmc::mapper::*;
 use mmc::mirroring;
+use nsf::NsfFile;
 
 pub struct NsfMapper {
     prg: MemoryBlock,
     chr: Vec<u8>,
+    nsf_player: Vec<u8>,
 
     mirroring: Mirroring,
     vram: Vec<u8>,
 }
 
+const NSF_PLAYER: [Opcode; 1] = [
+    Lda(Immediate(0x42))
+];
+
 impl NsfMapper {
     pub fn from_nsf(nsf: NsfFile) -> Result<NsfMapper, String> {
+        let mut nsf_player = assemble(Vec::from(NSF_PLAYER))?;
+        nsf_player.resize(0x1000, 0);
+
         return Ok(NsfMapper {
             prg: MemoryBlock::new(&nsf.prg, MemoryType::Ram),
             chr: vec![0u8; 0x2000],
+            nsf_player: nsf_player,
 
             mirroring: Mirroring::FourScreen,
             vram: vec![0u8; 0x1000],
@@ -36,7 +47,9 @@ impl Mapper for NsfMapper {
     
     fn debug_read_cpu(&self, address: u16) -> Option<u8> {
         match address {
-            /* none yet! */
+            0x5000 ..= 0x5FFF => Some(self.nsf_player[(address - 0x5000) as usize]),
+            0xFFFC => {Some(0x00)}, // reset vector
+            0xFFFD => {Some(0x50)},
             _ => None
         }
     }
