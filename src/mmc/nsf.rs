f20e278c0c9837e48300c72978bef6a84b7adcbc
Nicholas Flynt <thegamefreak0134@gmail.com>
2021-03-13 22:58:02 -0500

    Add MMC5 channels and support to NsfPlayer
    MMC5 is complex; for some reason we also have to include the
    multiply register and the PCM channel that, like, nobody ever
    uses. It's fine, really.

--- src/mmc/nsf.rs
+++ src/mmc/nsf.rs
@@ -16,6 +16,9 @@ use nsf::NsfFile;
 use mmc::vrc6::Vrc6PulseChannel;
 use mmc::vrc6::Vrc6SawtoothChannel;
 
+use apu::PulseChannelState;
+use mmc::mmc5::Mmc5PcmChannel;
+
 const PPUCTRL: u16 = 0x2000;
 const PPUMASK: u16 = 0x2001;
 const PPUSTATUS: u16 = 0x2002;
@@ -31,6 +34,9 @@ const COLOR_WHITE: u8 = 0x30;
 
 const PLAYER_COUNTER_COMPARE: u16 = 0x01FF;
 const PLAYER_PLAYBACK_COUNTER: u16 = 0x4900;
+const PLAYER_ORIGIN: u16 = 0x4A00;
+const PLAYER_SIZE: u16 = 0x0200;
+const PLAYER_END: u16 = PLAYER_ORIGIN + PLAYER_SIZE;
 
 
 fn wait_for_ppu_ready() -> Opcode {
@@ -146,7 +152,6 @@ pub struct NsfMapper {
     playback_period: f64,
     playback_counter: u8,
 
-
     mirroring: Mirroring,
     vram: Vec<u8>,
 
@@ -154,13 +159,22 @@ pub struct NsfMapper {
     vrc6_pulse1: Vrc6PulseChannel,
     vrc6_pulse2: Vrc6PulseChannel,
     vrc6_sawtooth: Vrc6SawtoothChannel,
+
+    mmc5_enabled: bool,
+    mmc5_multiplicand_a: u8,
+    mmc5_multiplicand_b: u8,
+    mmc5_pulse_1: PulseChannelState,
+    mmc5_pulse_2: PulseChannelState,
+    mmc5_audio_sequencer_counter: u16,
+    mmc5_pcm_channel: Mmc5PcmChannel,
+    mmc5_exram: Vec<u8>,
 }
 
 impl NsfMapper {
     pub fn from_nsf(nsf: NsfFile) -> Result<NsfMapper, String> {
         let nsf_player_opcodes = nsf_player(nsf.header.init_address(), nsf.header.play_address());
-        let mut nsf_player = assemble(nsf_player_opcodes, 0x5000)?;
-        nsf_player.resize(0x1000, 0);
+        let mut nsf_player = assemble(nsf_player_opcodes, PLAYER_ORIGIN)?;
+        nsf_player.resize(PLAYER_SIZE as usize, 0);
 
         let mut prg_rom = nsf.prg.clone();
         let mut prg_rom_banks = nsf.header.initial_banks();
@@ -193,6 +207,15 @@ impl NsfMapper {
             vrc6_pulse1: Vrc6PulseChannel::new("Pulse 1"),
             vrc6_pulse2: Vrc6PulseChannel::new("Pulse 2"),
             vrc6_sawtooth: Vrc6SawtoothChannel::new(),
+
+            mmc5_enabled: nsf.header.mmc5(),
+            mmc5_multiplicand_a: 0,
+            mmc5_multiplicand_b: 0,
+            mmc5_pulse_1: PulseChannelState::new("Pulse 1", "MMC5", 1_789_773, false),
+            mmc5_pulse_2: PulseChannelState::new("Pulse 2", "MMC5", 1_789_773, false),
+            mmc5_audio_sequencer_counter: 0,
+            mmc5_pcm_channel: Mmc5PcmChannel::new(),
+            mmc5_exram: vec![0u8; 0x400],
 
             prg_rom_banks: prg_rom_banks,
 
@@ -236,6 +259,199 @@ impl NsfMapper {
             _ => {}
         }
     }
+
+    pub fn clock_vrc6(&mut self) {
+        if self.vrc6_enabled {
+            self.vrc6_pulse1.clock();
+            self.vrc6_pulse2.clock();
+            self.vrc6_sawtooth.clock();
+        }
+    }
+
+    pub fn mmc5_write(&mut self, address: u16, data: u8) {
+        if !self.mmc5_enabled {
+            return;
+        }
+        let duty_table = [
+            0b1000_0000,
+            0b1100_0000,
+            0b1111_0000,
+            0b0011_1111,
+        ];
+        match address {
+            0x5000 => {
+                let duty_index =      (data & 0b1100_0000) >> 6;
+                let length_disable =  (data & 0b0010_0000) != 0;
+                let constant_volume = (data & 0b0001_0000) != 0;
+
+                self.mmc5_pulse_1.duty = duty_table[duty_index as usize];
+                self.mmc5_pulse_1.length_counter.halt_flag = length_disable;
+                self.mmc5_pulse_1.envelope.looping = length_disable;
+                self.mmc5_pulse_1.envelope.enabled = !(constant_volume);
+                self.mmc5_pulse_1.envelope.volume_register = data & 0b0000_1111;
+            },
+            0x5002 => {
+                let period_low = data as u16;
+                self.mmc5_pulse_1.period_initial = (self.mmc5_pulse_1.period_initial & 0xFF00) | period_low;
+            },
+            0x5003 => {
+                let period_high =  ((data & 0b0000_0111) as u16) << 8;
+                let length_index = (data & 0b1111_1000) >> 3;
+
+                self.mmc5_pulse_1.period_initial = (self.mmc5_pulse_1.period_initial & 0x00FF) | period_high;
+                self.mmc5_pulse_1.length_counter.set_length(length_index);
+
+                // Start this note
+                self.mmc5_pulse_1.sequence_counter = 0;
+                self.mmc5_pulse_1.envelope.start_flag = true;
+            },
+            0x5004 => {
+                let duty_index =      (data & 0b1100_0000) >> 6;
+                let length_disable =  (data & 0b0010_0000) != 0;
+                let constant_volume = (data & 0b0001_0000) != 0;
+
+                self.mmc5_pulse_2.duty = duty_table[duty_index as usize];
+                self.mmc5_pulse_2.length_counter.halt_flag = length_disable;
+                self.mmc5_pulse_2.envelope.looping = length_disable;
+                self.mmc5_pulse_2.envelope.enabled = !(constant_volume);
+                self.mmc5_pulse_2.envelope.volume_register = data & 0b0000_1111;
+            },
+            0x5006 => {
+                let period_low = data as u16;
+                self.mmc5_pulse_2.period_initial = (self.mmc5_pulse_2.period_initial & 0xFF00) | period_low;
+            },
+            0x5007 => {
+                let period_high =  ((data & 0b0000_0111) as u16) << 8;
+                let length_index =  (data & 0b1111_1000) >> 3;
+
+                self.mmc5_pulse_2.period_initial = (self.mmc5_pulse_2.period_initial & 0x00FF) | period_high;
+                self.mmc5_pulse_2.length_counter.set_length(length_index);
+
+                // Start this note
+                self.mmc5_pulse_2.sequence_counter = 0;
+                self.mmc5_pulse_2.envelope.start_flag = true;
+            },
+            0x5010 => {
+                self.mmc5_pcm_channel.read_mode =  (data & 0b0000_0001) != 0;
+                self.mmc5_pcm_channel.irq_enable =  (data & 0b1000_0000) != 0;
+            },
+            0x5011 => {
+                if !(self.mmc5_pcm_channel.read_mode) {
+                    self.mmc5_pcm_channel.level = data;
+                }
+            },
+            0x5015 => {
+                self.mmc5_pulse_1.length_counter.channel_enabled  = (data & 0b0001) != 0;
+                self.mmc5_pulse_2.length_counter.channel_enabled  = (data & 0b0010) != 0;
+              
+                if !(self.mmc5_pulse_1.length_counter.channel_enabled) {
+                    self.mmc5_pulse_1.length_counter.length = 0;
+                }
+                if !(self.mmc5_pulse_2.length_counter.channel_enabled) {
+                    self.mmc5_pulse_2.length_counter.length = 0;
+                }
+            }
+            0x5205 => {self.mmc5_multiplicand_a = data;},
+            0x5206 => {self.mmc5_multiplicand_b = data;},
+            0x5C00 ..= 0x5FF5 => {
+                self.mmc5_exram[(address - 0x5C00) as usize] = data;
+            }
+            _ => {}
+        }
+    }
+
+    fn clock_mmc5(&mut self) {
+        if !self.mmc5_enabled {
+            return;
+        }
+        self.mmc5_audio_sequencer_counter += 1;
+        if (self.mmc5_audio_sequencer_counter & 0b1) == 0 {
+            self.mmc5_pulse_1.clock();
+            self.mmc5_pulse_2.clock();
+        }
+        if self.mmc5_audio_sequencer_counter >= 7446 {
+            self.mmc5_pulse_1.envelope.clock();
+            self.mmc5_pulse_2.envelope.clock();
+            self.mmc5_pulse_1.length_counter.clock();
+            self.mmc5_pulse_2.length_counter.clock();
+            // Note: MMC5 pulse channels don't support sweep. We're borrowing the implementation
+            // from the underlying APU, but intentionally not clocking the sweep units.
+            self.mmc5_audio_sequencer_counter = 0;
+        }
+    }
+
+    fn snoop_mmc5(&mut self, address: u16) {
+        if !self.mmc5_enabled {
+            return;
+        }
+        // do the snoop PCM playback thing
+        if self.mmc5_pcm_channel.read_mode {
+            match address {
+                0x8000 ..= 0xBFFF => {
+                    self.mmc5_pcm_channel.level = self.debug_read_cpu(address).unwrap_or(0);
+                },
+                _ => {}
+            }
+        }
+    }
+
+    fn read_mmc5(&self, address: u16) -> Option<u8> {
+        if !self.mmc5_enabled {
+            return None;
+        }
+
+        // Handle MMC5 specific address spaces
+        match address {
+            0x5010 => {
+                let mut pcm_status = 0;
+                if self.mmc5_pcm_channel.read_mode {
+                    pcm_status |= 0b0000_0001;
+                }
+                if self.mmc5_pcm_channel.irq_pending {
+                    pcm_status |= 0b1000_0000;   
+                }
+                return Some(pcm_status)
+            },
+            0x5015 => {
+                let mut pulse_status = 0;
+                if self.mmc5_pulse_1.length_counter.length > 0 {
+                    pulse_status += 0b0000_0001;
+                }
+                if self.mmc5_pulse_2.length_counter.length > 0 {
+                    pulse_status += 0b0000_0010;
+                }
+                return Some(pulse_status);
+            },
+            0x5205 => {
+                let result = self.mmc5_multiplicand_a as u16 * self.mmc5_multiplicand_b as u16;
+                return Some((result & 0xFF) as u8);
+            },
+            0x5206 => {
+                let result = self.mmc5_multiplicand_a as u16 * self.mmc5_multiplicand_b as u16;
+                return Some(((result & 0xFF00) >> 8) as u8);
+            },
+            0x5C00 ..= 0x5FF5 => {
+                return Some(self.mmc5_exram[(address - 0x5C00) as usize]);
+            }
+            _ => return None
+        }
+    }
+
+    fn mmc5_output(&self) -> f64 {
+        if !self.mmc5_enabled {
+            return 0.0;
+        }
+        let pulse_1_output = (self.mmc5_pulse_1.output() as f64 / 15.0) - 0.5;
+        let pulse_2_output = (self.mmc5_pulse_2.output() as f64 / 15.0) - 0.5;
+        let mut pcm_output = (self.mmc5_pcm_channel.level as f64 / 256.0) - 0.5;
+        if self.mmc5_pcm_channel.muted {
+            pcm_output = 0.0;
+        }
+
+        return 
+            (pulse_1_output + pulse_2_output) * 0.12 + 
+            pcm_output * 0.25;
+    }
 }
 
 impl Mapper for NsfMapper {
@@ -250,16 +466,14 @@ impl Mapper for NsfMapper {
             self.playback_accumulator -= self.playback_period;
         }
 
-        if self.vrc6_enabled {
-            self.vrc6_pulse1.clock();
-            self.vrc6_pulse2.clock();
-            self.vrc6_sawtooth.clock();
-        }
+        self.clock_vrc6();
+        self.clock_mmc5();
     }
 
     fn mix_expansion_audio(&self, nes_sample: f64) -> f64 {
         return 
             self.vrc6_output() +
+            self.mmc5_output() +
             nes_sample;
     }
 
@@ -269,6 +483,11 @@ impl Mapper for NsfMapper {
             channels.push(&self.vrc6_pulse1);
             channels.push(&self.vrc6_pulse2);
             channels.push(&self.vrc6_sawtooth);
+        }
+        if self.mmc5_enabled {
+            channels.push(&self.mmc5_pulse_1);
+            channels.push(&self.mmc5_pulse_2);
+            channels.push(&self.mmc5_pcm_channel);
         }
         return channels;
     }
@@ -280,6 +499,11 @@ impl Mapper for NsfMapper {
             channels.push(&mut self.vrc6_pulse2);
             channels.push(&mut self.vrc6_sawtooth);
         }
+        if self.mmc5_enabled {
+            channels.push(&mut self.mmc5_pulse_1);
+            channels.push(&mut self.mmc5_pulse_2);
+            channels.push(&mut self.mmc5_pcm_channel);
+        }
         return channels;
     }
 
@@ -289,12 +513,28 @@ impl Mapper for NsfMapper {
             self.vrc6_pulse2.record_current_output();
             self.vrc6_sawtooth.record_current_output();
         }
+        if self.mmc5_enabled {
+            self.mmc5_pulse_1.record_current_output();
+            self.mmc5_pulse_2.record_current_output();
+            self.mmc5_pcm_channel.record_current_output();
+        }
     }
     
+    fn read_cpu(&mut self, address: u16) -> Option<u8> {
+        let data = self.debug_read_cpu(address);
+        self.snoop_mmc5(address);
+        return data;
+    }
+
     fn debug_read_cpu(&self, address: u16) -> Option<u8> {
+        match self.read_mmc5(address) {
+            Some(data) => return Some(data),
+            None => {}
+        }
+
         match address {
             PLAYER_PLAYBACK_COUNTER => Some(self.playback_counter),
-            0x5000 ..= 0x5FFF => Some(self.nsf_player[(address - 0x5000) as usize]),
+            PLAYER_ORIGIN ..= PLAYER_END => Some(self.nsf_player[(address - PLAYER_ORIGIN) as usize]),
             0x8000 ..= 0x8FFF => self.prg.banked_read(0x1000, self.prg_rom_banks[0], (address - 0x8000) as usize),
             0x9000 ..= 0x9FFF => self.prg.banked_read(0x1000, self.prg_rom_banks[1], (address - 0x9000) as usize),
             0xA000 ..= 0xAFFF => self.prg.banked_read(0x1000, self.prg_rom_banks[2], (address - 0xA000) as usize),
@@ -303,8 +543,8 @@ impl Mapper for NsfMapper {
             0xD000 ..= 0xDFFF => self.prg.banked_read(0x1000, self.prg_rom_banks[5], (address - 0xD000) as usize),
             0xE000 ..= 0xEFFF => self.prg.banked_read(0x1000, self.prg_rom_banks[6], (address - 0xE000) as usize),
             0xF000 ..= 0xFFF9 => self.prg.banked_read(0x1000, self.prg_rom_banks[7], (address - 0xF000) as usize),
-            0xFFFC => {Some(0x00)}, // reset vector
-            0xFFFD => {Some(0x50)},
+            0xFFFC => {Some(((PLAYER_ORIGIN & 0x00FF) >> 0) as u8)}, // reset vector
+            0xFFFD => {Some(((PLAYER_ORIGIN & 0xFF00) >> 8) as u8)},
             _ => None
         }
     }
@@ -323,6 +563,9 @@ impl Mapper for NsfMapper {
         }
         if self.vrc6_enabled {
             self.vrc6_write(address, data);
+        }
+        if self.mmc5_enabled {
+            self.mmc5_write(address, data);
         }
     }
 
