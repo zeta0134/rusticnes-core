991f46f26070c38a61f328b76caf651f50d93455
Nicholas Flynt <thegamefreak0134@gmail.com>
2021-07-24 19:58:57 -0400

    Hookup PRG ROM, RAM, and internal RAM reading and writing
    That completes the basic featureset, and N163 games now appear to
    be playable! Mainly we're missing audio, but things don't appear
    to otherwise be broken.

--- src/mmc/n163.rs
+++ src/mmc/n163.rs
@@ -22,8 +22,9 @@ pub struct Namco163 {
     pub nt_banks: Vec<u8>,
     pub prg_banks: Vec<u8>,
 
-    pub write_enable: u8,
-    pub sound_disable: bool,
+    pub internal_ram_addr: u8,
+    pub internal_ram_auto_increment: bool,
+    pub sound_enabled: bool,
     pub nt_ram_at_0000: bool,
     pub nt_ram_at_1000: bool,
 
@@ -50,8 +51,9 @@ impl Namco163 {
             nt_banks: vec![0u8; 4],
             prg_banks: vec![0u8; 3],
 
-            write_enable: 0, // upper nybble mismatch, will disable PRG RAM at boot
-            sound_disable: true,
+            internal_ram_addr: 0, // upper nybble mismatch, will disable PRG RAM at boot
+            internal_ram_auto_increment: false,
+            sound_enabled: false,
             nt_ram_at_0000: false,
             nt_ram_at_1000: false,
         })
@@ -74,6 +76,20 @@ impl Namco163 {
             self.chr.banked_write(0x400, bank_index as usize, address as usize, data);
         }
     }
+
+    pub fn prg_ram_write_enabled(&self, address: u16) -> bool {
+        if self.internal_ram_addr & 0xF0 != 0b0100_0000 {
+            return false;
+        }
+        let masked_address = address & 0xF800;
+        match masked_address {
+            0x6000 => (self.internal_ram_addr & 0b0000_0001) == 0,
+            0x6800 => (self.internal_ram_addr & 0b0000_0010) == 0,
+            0x7000 => (self.internal_ram_addr & 0b0000_0100) == 0,
+            0x7800 => (self.internal_ram_addr & 0b0000_1000) == 0,
+            _ => false
+        }
+    }
 }
 
 impl Mapper for Namco163 {
@@ -81,8 +97,40 @@ impl Mapper for Namco163 {
         return Mirroring::Horizontal;
     }
     
-    fn debug_read_cpu(&self, _: u16) -> Option<u8> {
-        return None;
+    fn debug_read_cpu(&self, address: u16) -> Option<u8> {
+        match address {
+            0x4800 ..= 0x4FFF => {
+                Some(self.internal_ram[self.internal_ram_addr as usize])
+            },
+            0x5000 ..= 0x57FF => {
+                let irq_low = (self.irq_counter & 0x00FF) as u8;
+                Some(irq_low)
+            },
+            0x5800 ..= 0x5FFF => {
+                let irq_high = ((self.irq_counter & 0xFF00) >> 8) as u8;
+                let irq_enabled = if self.irq_enabled {0x80} else {0x00};
+                Some(irq_high | irq_enabled)
+            },
+            0x6000 ..= 0x7FFF => self.prg_ram.wrapping_read(address as usize - 0x6000),
+            0x8000 ..= 0x9FFF => self.prg_rom.banked_read(0x2000, self.prg_banks[0] as usize, address as usize),
+            0xA000 ..= 0xBFFF => self.prg_rom.banked_read(0x2000, self.prg_banks[1] as usize, address as usize),
+            0xC000 ..= 0xDFFF => self.prg_rom.banked_read(0x2000, self.prg_banks[2] as usize, address as usize),
+            0xE000 ..= 0xFFFF => self.prg_rom.banked_read(0x2000, 0xFF, address as usize),
+            _ => {None}
+        }
+    }
+
+    fn read_cpu(&mut self, address: u16) -> Option<u8> {
+        let data = self.debug_read_cpu(address);
+        match address {
+            0x4800 ..= 0x4FFF => {
+                if self.internal_ram_auto_increment {
+                    self.internal_ram_addr = (self.internal_ram_addr + 1) & 0x7F;
+                }
+            },
+            _ => {}
+        }
+        return data;
     }
 
     fn debug_read_ppu(&self, address: u16) -> Option<u8> {
@@ -126,6 +174,12 @@ impl Mapper for Namco163 {
     fn write_cpu(&mut self, address: u16, data: u8) {
         let masked_address = address & 0xF800;
         match masked_address {
+            0x4800 => {
+                self.internal_ram[self.internal_ram_addr as usize] = data;
+                if self.internal_ram_auto_increment {
+                    self.internal_ram_addr = (self.internal_ram_addr + 1) & 0x7F;
+                }
+            },
             0x5000 => {
                 let irq_low = data as u16;
                 self.irq_counter = (self.irq_counter & 0xFF00) | irq_low;
@@ -136,6 +190,11 @@ impl Mapper for Namco163 {
                 self.irq_counter = (self.irq_counter & 0x00FF) | irq_high;
                 self.irq_enabled = (data & 0x80) != 0;
                 self.irq_pending = false;
+            },
+            0x6000 ..= 0x7FFF => {
+                if self.prg_ram_write_enabled(address) {
+                    self.prg_ram.wrapping_write(address as usize - 0x6000, data);
+                }
             },
             0x8000 => {self.chr_banks[0] = data;},
             0x8800 => {self.chr_banks[1] = data;},
@@ -149,6 +208,22 @@ impl Mapper for Namco163 {
             0xC800 => {self.nt_banks[1] = data;},
             0xD000 => {self.nt_banks[2] = data;},
             0xD800 => {self.nt_banks[3] = data;},
+            0xE000 => {
+                self.prg_banks[0] = data & 0b0011_1111;
+                self.sound_enabled = (data & 0b0100_0000) == 0;
+            },
+            0xE800 => {
+                self.prg_banks[1] = data & 0b0011_1111;
+                self.nt_ram_at_0000 = (data & 0b0100_0000) == 0;
+                self.nt_ram_at_1000 = (data & 0b1000_0000) == 0;
+            },
+            0xF000 => {
+                self.prg_banks[2] = data & 0b0011_1111;                
+            }
+            0xF800 => {
+                self.internal_ram_addr = data & 0x7F;
+                self.internal_ram_auto_increment = (data & 0b1000_0000) == 0;
+            }
             _ => {}
         }
     }
@@ -164,5 +239,17 @@ impl Mapper for Namco163 {
 
     fn irq_flag(&self) -> bool {
         return self.irq_pending;
+    }
+
+    fn has_sram(&self) -> bool {
+        return true;
+    }
+
+    fn get_sram(&self) -> Vec<u8> {
+        return self.prg_ram.as_vec().clone();
+    }
+
+    fn load_sram(&mut self, sram_data: Vec<u8>) {
+        *self.prg_ram.as_mut_vec() = sram_data;
     }
 }
