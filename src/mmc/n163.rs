0ee11bf842ec1b9b2198ea7f282bc38b6ed28c59
Nicholas Flynt <thegamefreak0134@gmail.com>
2021-07-24 18:18:02 -0400

    Block out internal N163 registers, excluding audio for now

--- src/mmc/n163.rs
+++ src/mmc/n163.rs
@@ -5,14 +5,52 @@ use ines::INesCartridge;
 use memoryblock::MemoryBlock;
 
 use mmc::mapper::*;
-use mmc::mirroring;
 
 pub struct Namco163 {
+    pub prg_rom: MemoryBlock,
+    pub prg_ram: MemoryBlock,
+    pub chr: MemoryBlock,
+    pub vram: Vec<u8>,
+    pub internal_ram: Vec<u8>,
+
+    pub irq_enabled: bool,
+    pub irq_counter: u16, // 15bit, actually
+
+    pub chr_banks: Vec<u8>,
+    pub nt_banks: Vec<u8>,
+    pub prg_banks: Vec<u8>,
+
+    pub write_enable: u8,
+    pub sound_disable: bool,
+    pub nt_ram_at_0000: bool,
+    pub nt_ram_at_1000: bool,
+
 }
 
 impl Namco163 {
-    pub fn from_ines(_ines: INesCartridge) -> Result<Namco163, String> {
+    pub fn from_ines(ines: INesCartridge) -> Result<Namco163, String> {
+        let prg_rom_block = ines.prg_rom_block();
+        let prg_ram_block = ines.prg_ram_block()?;
+        let chr_block = ines.chr_block()?;
+
         return Ok(Namco163 {
+            prg_rom: prg_rom_block.clone(),
+            prg_ram: prg_ram_block.clone(),
+            chr: chr_block.clone(),
+            vram: vec![0u8; 0x2000],
+            internal_ram: vec![0u8; 0x80],
+
+            irq_counter: 0,
+            irq_enabled: false,
+
+            chr_banks: vec![0u8; 8],
+            nt_banks: vec![0u8; 4],
+            prg_banks: vec![0u8; 3],
+
+            write_enable: 0, // upper nybble mismatch, will disable PRG RAM at boot
+            sound_disable: true,
+            nt_ram_at_0000: false,
+            nt_ram_at_1000: false,
         })
     }
 }
