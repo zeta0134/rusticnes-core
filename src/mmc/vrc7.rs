68ebd869fc495ac5ec0683214744d85da74c1c31
Nicholas Flynt <thegamefreak0134@gmail.com>
2023-06-15 23:07:03 -0400

    Flesh out enough VRC7 details to get basic carrier sines playing
    Whoa!

--- src/mmc/vrc7.rs
+++ src/mmc/vrc7.rs
@@ -27,6 +27,8 @@ pub struct Vrc7 {
     pub irq_pending: bool,
     pub irq_counter: u8,
 
+    pub audio_register: u8,
+
     pub audio: Vrc7Audio,
 }
 
@@ -55,6 +57,7 @@ impl Vrc7 {
             irq_counter: 0,
 
             audio: Vrc7Audio::new(),
+            audio_register: 0,
         });
     }
 
@@ -91,6 +94,12 @@ impl Mapper for Vrc7 {
                 self._clock_irq_counter();
             }
         }
+        self.audio.clock();
+    }
+
+    fn mix_expansion_audio(&self, nes_sample: f32) -> f32 {
+        let combined_vrc7_audio = self.audio.output() as f32 / 256.0 / 6.0;
+        return combined_vrc7_audio + nes_sample;
     }
 
     fn irq_flag(&self) -> bool {
@@ -117,9 +126,9 @@ impl Mapper for Vrc7 {
             0x6000 ..= 0x7FFF => {self.prg_ram.wrapping_write((address - 0x6000) as usize, data);},
             0x8000 ..= 0xFFFF => {
                 let register_mask = match self.submapper {
-                    1 => 0xF008,
-                    2 => 0xF010,
-                    _ => 0xF010
+                    1 => 0xF028,
+                    2 => 0xF030,
+                    _ => 0xF030
                 };
                 let register_address = address & register_mask;
                 match register_address {
@@ -134,7 +143,12 @@ impl Mapper for Vrc7 {
                     0xC008 | 0xC010 => {self.chr_banks[5] = data},
                     0xD000          => {self.chr_banks[6] = data},
                     0xD008 | 0xD010 => {self.chr_banks[7] = data},
-                    // Sound would go here!!
+                    0x9010          => {
+                        self.audio_register = data
+                    },
+                    0x9030          => {
+                        self.audio.write(self.audio_register, data);
+                    },
                     0xE000         => {
                         match data & 0b0000_0011 {
                             0 => self.mirroring = Mirroring::Vertical,
@@ -214,6 +228,18 @@ impl Mapper for Vrc7 {
             _ => {}
         }
     }
+
+    fn has_sram(&self) -> bool {
+        return true;
+    }
+
+    fn get_sram(&self) -> Vec<u8> {
+        return self.prg_ram.as_vec().clone();
+    }
+
+    fn load_sram(&mut self, sram_data: Vec<u8>) {
+        *self.prg_ram.as_mut_vec() = sram_data;
+    }
 }
 
 // TODO: explore and see if we can't somehow make these constant while keeping them
@@ -237,13 +263,18 @@ fn generate_exp_table() -> Vec<u16> {
     return exp_lut;
 }
 
+pub const MT_LUT: [u32; 16] = [1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 20, 24, 24, 30, 30];
+
 pub struct Vrc7AudioChannel {
     fnum: u32,
     octave: u32,
     carrier_phase: u32,
-    volume: u8,
+    carrier_multiplier: usize,
+    volume: u16,
     logsin_lut: Vec<u16>,
     exp_lut: Vec<u16>,
+
+    debug_key_on: bool,
 }
 
 impl Vrc7AudioChannel {
@@ -252,9 +283,12 @@ impl Vrc7AudioChannel {
             fnum: 0,
             octave: 0,
             carrier_phase: 0,
+            carrier_multiplier: 0,
             volume: 0,
             logsin_lut: generate_logsin_lut(),
             exp_lut: generate_exp_table(),
+
+            debug_key_on: false,
         };
     }
 
@@ -287,12 +321,16 @@ impl Vrc7AudioChannel {
     }
 
     pub fn update(&mut self) {
-
+        let step_size = ((self.fnum * MT_LUT[self.carrier_multiplier]) << self.octave) >> 1;
+        self.carrier_phase = (self.carrier_phase + step_size) & 0x7FFFF;
     }
 
     pub fn output(&self) -> i16 {
-        // TODO
-        return 0;
+        if self.debug_key_on {
+            return self.adjusted_sine((self.carrier_phase >> 9) as usize, self.volume, 0);
+        } else {
+            return 0;
+        }
     }
 }
 
@@ -303,6 +341,8 @@ pub struct Vrc7Audio {
     pub channel4: Vrc7AudioChannel,
     pub channel5: Vrc7AudioChannel,
     pub channel6: Vrc7AudioChannel,
+    pub current_channel: usize,
+    pub delay_counter: u8,
 }
 
 impl Vrc7Audio {
@@ -314,10 +354,100 @@ impl Vrc7Audio {
             channel4: Vrc7AudioChannel::new(),
             channel5: Vrc7AudioChannel::new(),
             channel6: Vrc7AudioChannel::new(),
+            current_channel: 1,
+            delay_counter: 0,
         };
     }
 
-    pub fn clock() {
+    pub fn clock(&mut self) {
+        if self.delay_counter == 0 {
+            match self.current_channel {
+                0 => self.channel1.update(),
+                1 => self.channel2.update(),
+                2 => self.channel3.update(),
+                3 => self.channel4.update(),
+                4 => self.channel5.update(),
+                5 => self.channel6.update(),
+                _ => {} // unreachable
+            }
+            self.current_channel += 1;
+            if self.current_channel >= 6 {
+                self.current_channel = 0;
+            }
+            self.delay_counter = 5;
+        } else {
+            self.delay_counter -= 1;
+        }
+    }
 
+    pub fn output(&self) -> i16 {
+        let combined_output = 
+            self.channel1.output() + 
+            self.channel2.output() + 
+            self.channel3.output() + 
+            self.channel4.output() + 
+            self.channel5.output() + 
+            self.channel6.output();
+        return combined_output;
+    }
+
+    pub fn write(&mut self, address: u8, data: u8) {
+        match address {
+            0x10 => {self.channel1.fnum = (self.channel1.fnum & 0xFF00) + (data as u32)},
+            0x11 => {self.channel2.fnum = (self.channel2.fnum & 0xFF00) + (data as u32)},
+            0x12 => {self.channel3.fnum = (self.channel3.fnum & 0xFF00) + (data as u32)},
+            0x13 => {self.channel4.fnum = (self.channel4.fnum & 0xFF00) + (data as u32)},
+            0x14 => {self.channel5.fnum = (self.channel5.fnum & 0xFF00) + (data as u32)},
+            0x15 => {self.channel6.fnum = (self.channel6.fnum & 0xFF00) + (data as u32)},
+            0x20 => {
+                self.channel1.fnum = (self.channel1.fnum & 0x00FF) + (((data & 0b1) as u32) << 8);
+                self.channel1.octave = ((data & 0b1110) >> 1) as u32;
+                self.channel1.debug_key_on = (data & 0b1_0000) != 0;
+            },
+            0x21 => {
+                self.channel2.fnum = (self.channel2.fnum & 0x00FF) + (((data & 0b1) as u32) << 8);
+                self.channel2.octave = ((data & 0b1110) >> 1) as u32;
+                self.channel2.debug_key_on = (data & 0b1_0000) != 0;
+            },
+            0x22 => {
+                self.channel3.fnum = (self.channel3.fnum & 0x00FF) + (((data & 0b1) as u32) << 8);
+                self.channel3.octave = ((data & 0b1110) >> 1) as u32;
+                self.channel3.debug_key_on = (data & 0b1_0000) != 0;
+            },
+            0x23 => {
+                self.channel4.fnum = (self.channel4.fnum & 0x00FF) + (((data & 0b1) as u32) << 8);
+                self.channel4.octave = ((data & 0b1110) >> 1) as u32;
+                self.channel4.debug_key_on = (data & 0b1_0000) != 0;
+            },
+            0x24 => {
+                self.channel5.fnum = (self.channel5.fnum & 0x00FF) + (((data & 0b1) as u32) << 8);
+                self.channel5.octave = ((data & 0b1110) >> 1) as u32;
+                self.channel5.debug_key_on = (data & 0b1_0000) != 0;
+            },
+            0x25 => {
+                self.channel6.fnum = (self.channel6.fnum & 0x00FF) + (((data & 0b1) as u32) << 8);
+                self.channel6.octave = ((data & 0b1110) >> 1) as u32;
+                self.channel6.debug_key_on = (data & 0b1_0000) != 0;
+            },
+            0x30 => {
+                self.channel1.volume = (data & 0xF) as u16;
+            },
+            0x31 => {
+                self.channel2.volume = (data & 0xF) as u16;
+            },
+            0x32 => {
+                self.channel3.volume = (data & 0xF) as u16;
+            },
+            0x33 => {
+                self.channel4.volume = (data & 0xF) as u16;
+            },
+            0x34 => {
+                self.channel5.volume = (data & 0xF) as u16;
+            },
+            0x35 => {
+                self.channel6.volume = (data & 0xF) as u16;
+            },
+            _ => {}
+        }
     }
 }
